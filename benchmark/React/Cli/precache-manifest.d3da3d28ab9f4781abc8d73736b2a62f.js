5720e0fe3a28443ac63ccd2b75d7ad2fb3e26bfe
santiagoExpansiva <santiago@expansiva.com.br>
2019-11-11 16:43:34 -0300

    teste

---
+++ benchmark/React/Cli/precache-manifest.d3da3d28ab9f4781abc8d73736b2a62f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cfb19a70877dcd7ba16eed52875a2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dcaeb5d3cd2fb430ce6",
+    "url": "/static/js/2.cc786c44.chunk.js"
+  },
+  {
+    "revision": "95f651d1afcabf011490",
+    "url": "/static/js/main.ef2fe7c8.chunk.js"
+  },
+  {
+    "revision": "1d0a75400a5bfb472e7d",
+    "url": "/static/js/runtime-main.15ba8310.js"
+  }
+]);
