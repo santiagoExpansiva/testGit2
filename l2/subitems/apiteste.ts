de48b366b6cb5ec15c953efe9cde13db7e05685e
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2023-01-19 15:16:35 -0300

    teste

---
+++ l2/subitems/apiteste.ts
@@ -0,0 +1,26 @@
+class _100167_apiteste extends l2_html.Base {
+
+	// Copyright multilevelstudio.com, or its affiliates. All Rights Reserved.
+	// SPDX-License-Identifier: MIT-0
+
+	public render(): HTMLElement {
+
+		super.createEL('null'); // create this.el t
+		l2_fieldTypes.renderAllFields(this.el, this);
+		return this.el;
+
+	}
+
+	// 3 - Search Page
+
+	public static readonly _SearchDetails: l2_html.SearchType = {
+		section: l2_html.PCategories.dataManagement,
+		group: 'js',
+		desc: 'description here',
+		tags: ['l2'],
+		childrenTags: ['!'],
+		examples: [
+		]
+	}
+
+}
